fcbf3a820fec4c461b98505f52b855121aa175a0
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-31 22:36:24 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home'),),
+      body: Center(
+        child: Text("Home Page",
+        style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
