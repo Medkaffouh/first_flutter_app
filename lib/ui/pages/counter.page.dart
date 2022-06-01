afb6fbcf195eef6b67e5e87e05c3c40d809d8985
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-01 11:03:17 +0100

    increment and decrement the counter page

--- lib/ui/pages/counter.page.dart
+++ lib/ui/pages/counter.page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-class CounterPage extends StatelessWidget {
+class CounterPage extends StatefulWidget {
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
   int counter=0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,13 +14,29 @@ class CounterPage extends StatelessWidget {
       body: Center(
         child: Text('Counter Value ${counter}',style: Theme.of(context).textTheme.headline3,),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          ++counter;
-          print(counter);
-        },
-
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () {
+              setState((){
+                --counter;
+              });
+              print(counter);
+            },
+          ),
+          SizedBox(width: 8,),
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState((){
+                ++counter;
+              });
+              print(counter);
+            },
+          )
+        ],
       ),
     );
   }
